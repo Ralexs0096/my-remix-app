d6f08f8a844ba633d5c12b6664682bfad28d8a30
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-01 01:20:50 -0600

    Add nesting routes for settings page

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,8 +1,15 @@
+import { Link, Outlet } from '@remix-run/react';
+
 export default function Settings() {
   return (
     <div>
       <h1>Settings</h1>
       <p>This is the Settings page</p>
+      <nav>
+        <Link to="app">App</Link>
+        <Link to="profile">profile</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
