c7255bb44d538d3c5fdf8f1fa1f80b30d9f72f97
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-01 00:58:16 -0600

    Adjust vite config to use v1 route convention

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { createRoutesFromFolders } from '@remix-run/v1-route-convention';
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
     v3_singleFetch: true;
   }
@@ -16,9 +17,16 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
+        v3_lazyRouteDiscovery: true
       },
+      ignoredRouteFiles: ['**/.*'],
+      routes(defineRoutes) {
+        return createRoutesFromFolders(defineRoutes);
+      }
     }),
-    tsconfigPaths(),
+    tsconfigPaths()
   ],
+  server: {
+    port: 3000
+  }
 });
